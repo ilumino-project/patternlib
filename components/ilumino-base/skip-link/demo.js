c94e8074264eb970c14c05d2c5d3ddf15d7e9691
srayos <stacey@ilumino.co>
2020-11-03 08:52:23 -0600

    update demo js

--- components/ilumino-base/skip-link/demo.js
+++ components/ilumino-base/skip-link/demo.js
@@ -1,5 +1,5 @@
 module.exports = {
-    html: () => '<div id="skiplinks"><a href="mainContent">Skip to Main Content</a></div>',
+    html: () => '<div id="skiplinks"><a href="mainContent">Skip to Main Content</a></div><nav id="mainMenu">Main Menu Here</nav><div id="mainContent"><h1>Main Content Area</h1><p>Not a sunrise but a galaxyrise galaxies how far away adipisci velit citizens of distant epochs decipherment. Ship of the imagination are creatures of the cosmos Sea of Tranquility vel illum qui dolorem eum fugiat quo voluptas nulla pariatur vel illum qui dolorem eum fugiat quo voluptas nulla pariatur sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. A still more glorious dawn awaits Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium dream of the mind\'s eye encyclopaedia galactica Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium two ghostly white figures in coveralls and helmets are softly dancing and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p></div>',
     css: () => `
       #skiplinks a {
           position: absolute;
@@ -16,8 +16,5 @@ module.exports = {
           outline: 1px dashed #000;
       }
       ',
-      `,
-    default: () => {
-        // tbd
-    }
+      `
 }
